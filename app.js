f3b5c0c40c05f492d1ce331f5597c7c6888d1e63
raineer <delaritarainer81@gmail.com>
2018-07-25 16:18:07 +0800

    log.info server started

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+const config = require('./config/config');
+const log = require('color-logs')(true, true, __filename);
+
+app.listen(config.env.port, '0.0.0.0', () => {
+    log.info(`Server started on ${config.env.port}`);
+});
---
+++ config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+    env: {
+        port: process.env.port || '7001',
+    }
+}
