c77f17a8fa42e7ba1219110a973092bacfea0cc9
raineer <delaritarainer81@gmail.com>
2018-07-26 15:32:55 +0800

    put update useraccount\

--- app.js
+++ app.js
@@ -30,3 +30,19 @@ app.get('/useraccount/:id', function (req, res) {
         res.end(JSON.stringify(results));
     });
 });
+
+//rest api to create a new record into mysql database
+app.post('/useraccount', function (req, res) {
+    var postData = req.body;
+    Conn.query('INSERT INTO useraccount SET ?', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
+//rest api to update record into mysql database
+app.put('/useraccount', function (req, res) {
+    Conn.query('UPDATE `useraccount` SET `username`=?,`password`=?,`email`=? where `id`=?', [req.body.username, req.body.password, req.body.email, req.body.id], function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+});
